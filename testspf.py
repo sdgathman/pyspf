fafc26df5bfac387cb91b4412759cfda4e15dae8
Scott Kitterman <spf2@kitterman.com>
2006-10-07 22:22:55 +0000

    Update 1.7 branch test suite from head.

--- testspf.py
+++ testspf.py
@@ -1,8 +1,12 @@
 import unittest
+import socket
+import sys
 import spf
 import csv
 import re
 import yaml
+# yaml can be found at http://pyyaml.org/
+# Tested with PYYAML 3.04
 
 zonedata = {}
 RE_IP4 = re.compile(r'\.'.join(
@@ -12,7 +16,17 @@ def DNSLookup(name,qtype):
   try:
     #print name
     timeout = True
-    for i in zonedata[name]:
+
+    # emulate pydns-2.3.0 label processing
+    a = []
+    for label in name.split('.'):
+      if label:
+        if len(label) > 63:
+          raise spf.TempError,'DNS label too long'
+        a.append(label)
+    name = '.'.join(a)
+
+    for i in zonedata[name.lower()]:
       if i == 'TIMEOUT':
         if timeout:
 	  raise spf.TempError,'DNS timeout'
@@ -20,6 +34,13 @@ def DNSLookup(name,qtype):
       t,v = i
       if t == qtype:
         timeout = False
+      if v == 'TIMEOUT':
+        if t == qtype:
+	  raise spf.TempError,'DNS timeout'
+	continue
+      # keep test zonedata human readable, but translate to simulate pydns
+#      if t == 'AAAA': comment out IPv6 related tests since v1.7 doesn't do v6.
+#	v = spf.inet_pton(v)
       yield ((name,t),v)
   except KeyError:
     if name.startswith('error.'):
@@ -32,7 +53,10 @@ class SPFTest(object):
     self.id = testid
     self.scenario = scenario
     self.explanation = None
+    self.spec = None
     self.comment = []
+    if 'result' not in data:
+      print testid,'missing result'
     for k,v in data.items():
       setattr(self,k,v)
     if type(self.comment) is str:
@@ -40,12 +64,23 @@ class SPFTest(object):
 
 def getrdata(r):
   "Unpack rdata given as list of maps to list of tuples."
+  txt = []	# generated TXT records
+  gen = True
   for m in r:
     try:
-      for t in m.items():
-        yield t
+      for i in m.items():
+        t,v = i
+        if t == 'TXT':
+	  gen = False # no generated TXT records
+	elif t == 'SPF' and gen:
+	  txt.append(('TXT',v))
+	if v != 'NONE':
+	  yield i
     except:
       yield m
+  if gen:
+    for i in txt:
+      yield i
 
 class SPFScenario(object):
   def __init__(self,filename=None,data={}):
@@ -56,7 +91,7 @@ class SPFScenario(object):
     self.tests = {}
     if data:
       self.zonedata= dict([
-        (d, list(getrdata(r))) for d,r in data['zonedata'].items()
+        (d.lower(), list(getrdata(r))) for d,r in data['zonedata'].items()
       ])
       for t,v in data['tests'].items():
         self.tests[t] = SPFTest(t,self,v)
@@ -72,14 +107,14 @@ class SPFScenario(object):
     self.tests[test.id] = test
 
 def loadYAML(fname):
-  "Load testcases in YAML format.  Return list of SPFTest"
+  "Load testcases in YAML format.  Return map of SPFTests by name."
   fp = open(fname,'rb')
   tests = {}
   for s in yaml.safe_load_all(fp):
     scenario = SPFScenario(fname,data=s)
     for k,v in scenario.tests.items():
       tests[k] = v
-  return tests.values()
+  return tests
 
 def loadBind(fname):
   "Load testcases in BIND format.  Return list of SPFTest"
@@ -140,27 +175,37 @@ def loadBind(fname):
 
 oldresults = { 'unknown': 'permerror', 'error': 'temperror' }
 
+verbose = 0
+
 class SPFTestCase(unittest.TestCase):
 
-  def runTests(self,tests):
+  def runTest(self,tests):
     global zonedata
     passed,failed = 0,0
     for t in tests:
       zonedata = t.scenario.zonedata
       q = spf.query(i=t.host, s=t.mailfrom, h=t.helo)
+      q.set_default_explanation('DEFAULT')
       res,code,exp = q.check()
       if res in oldresults:
         res = oldresults[res]
-      ok = res == t.result
+      ok = True
+      if res != t.result and res not in t.result:
+        if verbose: print t.result,'!=',res
+	ok = False
+      elif res != t.result and res != t.result[0]:
+        print "WARN: %s in %s, %s: %s preferred to %s" % (
+		t.id,t.scenario.filename,t.spec,t.result[0],res)
       if t.explanation is not None and t.explanation != exp:
-        print t.explanation,'!=',exp
+        if verbose: print t.explanation,'!=',exp
         ok = False
       if ok:
 	passed += 1
       else:
-        print t.result,'!=',res
 	failed += 1
-	print "test %s in %s failed" % (t.id,t.scenario.filename)
+	print "%s in %s failed, %s" % (t.id,t.scenario.filename,t.spec)
+	if verbose and not t.explanation: print exp
+	if verbose > 1: print t.scenario.zonedata
     if failed:
       print "%d passed" % passed,"%d failed" % failed
 
@@ -171,12 +216,22 @@ class SPFTestCase(unittest.TestCase):
   #  self.runTests(loadBind('otest.dat'))
 
   def testYAML(self):
-    self.runTests(loadYAML('test.yml'))
+    self.runTest(loadYAML('test.yml').values())
 
   def testRFC(self):
-    self.runTests(loadYAML('rfc4408-tests.yml'))
+    self.runTest(loadYAML('rfc4408-tests.yml').values())
 
 def suite(): return unittest.makeSuite(SPFTestCase,'test')
 
 if __name__ == '__main__':
-  unittest.main()
+  tc = None
+  for i in sys.argv[1:]:
+    if i == '-v':
+      verbose += 1
+      continue
+    if not tc:
+      tc = SPFTestCase()
+      t = loadYAML('rfc4408-tests.yml')
+    tc.runTest([t[i]])
+  if not tc:
+    unittest.main()
