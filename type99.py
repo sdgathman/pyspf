925f3b514b5cb2b1301657047cfabbc41a3af2d3
Scott Kitterman <spf2@kitterman.com>
2007-01-25 21:59:29 +0000

    Update comments to match bug fix.  Include copyright statements.  Update sheband.

--- type99.py
+++ type99.py
@@ -1,15 +1,37 @@
-#!/usr/bin/python2.4
-#
+#!/usr/bin/env python
+"""Type 99 (SPF) DNS conversion script.
+
+Copyright (c) 2005,2006 Stuart Gathman <stuart@bmsi.com>
+Portions Copyright (c) 2007 Scott Kitterman <scott@kitterman.com>
+This module is free software, and you may redistribute it and/or modify
+it under the same terms as Python itself, so long as this copyright message
+and disclaimer are retained in their original form.
+
+IN NO EVENT SHALL THE AUTHOR BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF
+THIS CODE, EVEN IF THE AUTHOR HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+
+THE AUTHOR SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE.  THE CODE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS,
+AND THERE IS NO OBLIGATION WHATSOEVER TO PROVIDE MAINTENANCE,
+SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+For more information about SPF, a tool against email forgery, see
+    http://www.openspf.org/"""
+    
 # Copy Bind zonefiles to stdout, removing TYPE99 RRs and
 # adding a TYPE99 RR for each TXT RR encountered.
 # This can be used to maintain SPF records as TXT RRs
 # in a zonefile until Bind is patched/upgraded to recognize
 # the SPF RR.  After adding/changing/deleting TXT RRs,
 # filtering through this script will refresh the TYPE99 RRs.
-#
-# BUGS: multiline TXT RRs are not supported.
 # 
 # $Log$
+# Revision 1.6  2007/01/25 21:51:45  kitterma
+# Fix type99 script for multi-line support (Fixes sourceforge #1257140)
+#
 # Revision 1.5  2006/12/16 20:45:23  customdesigned
 # Move dns drivers to package directory.
 #
