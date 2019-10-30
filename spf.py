28e0207e3dbe4a22e346f0c57c18d6cb8a014767
Scott Kitterman <scott@kitterman.com>
2019-10-30 18:20:11 -0400

    Add missing use of timeout parameter for dnspython DNSLookup

--- spf.py
+++ spf.py
@@ -128,7 +128,7 @@ def DNSLookup_dnspython(name, qtype, tcpfallback=True, timeout=30):
     retVal = []
     try:
         # FIXME: how to disable TCP fallback in dnspython if not tcpfallback?
-        answers = dns.resolver.query(name, qtype)
+        answers = dns.resolver.query(name, qtype, lifetime=timeout)
         for rdata in answers:
             if qtype == 'A' or qtype == 'AAAA':
                 retVal.append(((name, qtype), rdata.address))
